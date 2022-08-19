816ba3588b711663e5a9cd12b2d369e92faf133b
Steve Kuznetsov <skuznets@redhat.com>
2022-08-19 15:51:42 -0600

    components/map: add the danger scale legend to the map
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- components/Map.tsx
+++ components/Map.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import MapView, {Region} from 'react-native-maps';
 
+import Color from 'color';
+
+import {DangerLevel, dangerText} from '../types/nationalAvalancheCenter';
 import {AvalancheCenterForecastZonePolygons} from './AvalancheCenterForecastZonePolygons';
+import {colorFor} from './AvalancheDangerPyramid';
 
 const defaultRegion: Region = {
   // TODO(skuznets): add a sane default for the US?
@@ -44,7 +48,22 @@ export const Map: React.FunctionComponent<MapProps> = ({centers, date}: MapProps
         {isReady && centers.map(center_id => <AvalancheCenterForecastZonePolygons key={center_id} center_id={center_id} setRegion={setRegion} date={date} />)}
       </MapView>
       <View style={styles.legend}>
-        <Text>help</Text>
+        <Text style={styles.legendTitle}>Avalanche Danger Scale</Text>
+        <View style={styles.legendItems}>
+          {Object.keys(DangerLevel)
+            .filter(key => Number.isNaN(+key))
+            .filter(key => DangerLevel[key] != DangerLevel.None)
+            .map(key => {
+              const level: DangerLevel = DangerLevel[key];
+              const backgroundColor: Color = colorFor(level).alpha(0.85);
+              const textColor: Color = backgroundColor.isLight() ? Color('black') : Color('white');
+              return (
+                <View key={key} style={{...styles.legendMarker, backgroundColor: backgroundColor.string()}}>
+                  <Text style={{color: textColor.string()}}>{dangerText(DangerLevel[key])}</Text>
+                </View>
+              );
+            })}
+        </View>
       </View>
     </>
   );
@@ -55,7 +74,44 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
   },
   legend: {
+    backgroundColor: 'white',
+    padding: 2,
+    margin: 2,
+    borderStyle: 'solid',
+    borderWidth: 1.2,
+    borderColor: 'rgb(200,202,206)',
+    shadowOffset: {width: 1, height: 2},
+    shadowOpacity: 0.8,
+    shadowColor: 'rgb(157,162,165)',
+    borderRadius: 5,
     position: 'absolute',
-    bottom: 50,
+    width: '100%',
+    bottom: 5,
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'column',
+  },
+  legendTitle: {
+    padding: 2,
+    fontWeight: 'bold',
+  },
+  legendItems: {
+    width: '100%',
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  legendMarker: {
+    padding: 2,
+    marginLeft: 2,
+    marginRight: 2,
+    borderStyle: 'solid',
+    borderWidth: 1.2,
+    borderColor: 'rgb(200,202,206)',
+    shadowOffset: {width: 1, height: 2},
+    shadowOpacity: 0.8,
+    shadowColor: 'rgb(157,162,165)',
+    borderRadius: 5,
   },
 });
