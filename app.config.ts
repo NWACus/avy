209d8e21deb69742179a676481f69498eb0d7e87
Brian Sharon <brian@floatplane.us>
2022-12-07 10:16:59 -0800

    Fix some tsc errors

--- app.config.ts
+++ app.config.ts
@@ -1,15 +1,21 @@
 import {ExpoConfig, ConfigContext} from '@expo/config';
 
-export default ({config}: ConfigContext): ExpoConfig => {
-  if (!config.ios.config) {
-    config.ios.config = {};
-  }
-  config.ios.config.googleMapsApiKey = process.env.IOS_GOOGLE_MAPS_API_KEY;
-  if (!config.android.config) {
-    config.android.config = {};
-  }
-  config.android.config.googleMaps = {apiKey: process.env.ANDROID_GOOGLE_MAPS_API_KEY};
-  return {
-    ...config,
-  };
+export default ({config}: ConfigContext): Partial<ExpoConfig> => {
+    return {
+        ...config,
+        ios: {
+            ...config.ios,
+            config: {
+                ...config.ios?.config,
+                googleMapsApiKey: process.env.IOS_GOOGLE_MAPS_API_KEY,
+            },
+        },
+        android: {
+            ...config.android,
+            config: {
+                ...config.android?.config,
+                googleMaps: {apiKey: process.env.ANDROID_GOOGLE_MAPS_API_KEY},
+            },
+        },
+    }
 };
