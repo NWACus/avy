dd3e10277f21cbe116624a717aa3001a6fa021c1
Brian Sharon <brian@nwac.us>
2023-01-04 15:17:44 -0800

    new danger scale

--- components/DangerScale.tsx
+++ components/DangerScale.tsx
@@ -1,94 +1,44 @@
 import React from 'react';
 
-import {StyleSheet, Text, View, ViewStyle} from 'react-native';
-import {FontAwesome5} from '@expo/vector-icons';
+import {StyleSheet, View, ViewStyle} from 'react-native';
+import {MaterialIcons} from '@expo/vector-icons';
 
 import {DangerLevel} from 'types/nationalAvalancheCenter';
 import {colorFor} from './AvalancheDangerPyramid';
 import {dangerShortText} from './helpers/dangerText';
+import {Center, HStack, Text} from 'native-base';
 
-export interface DangerScaleProps {
-  style?: ViewStyle;
-}
+export interface DangerScaleProps {}
 
-export const DangerScale: React.FunctionComponent<DangerScaleProps> = ({style}: DangerScaleProps) => {
+export const DangerScale: React.FunctionComponent<DangerScaleProps> = () => {
   return (
-    <View style={{...styles.legend, ...style}}>
-      <View style={styles.legendHeader}>
-        <Text style={styles.legendTitle}>Danger Scale</Text>
-        <FontAwesome5 name="info-circle" size={16} color="blue" />
-      </View>
-      <View style={styles.legendItems}>
+    <HStack backgroundColor="rgba(0, 0, 0, 0.6)" borderRadius={24} px="4" py="2" justifyContent="space-between" alignItems="center">
+      <HStack space="1" alignItems="center">
+        <Text color="white" bold style={{letterSpacing: -0.61}}>
+          Danger Scale
+        </Text>
+        <MaterialIcons name="open-in-new" size={16} color="white" />
+      </HStack>
+      <HStack space={0} justifyContent="stretch">
         {Object.keys(DangerLevel)
           .filter(key => Number.isNaN(+key))
           .filter(key => DangerLevel[key] > DangerLevel.None)
           .map(key => DangerLevel[key])
           .map(level => (
-            <View
-              key={level}
+            <Center
               style={{
-                ...styles.legendColor,
-                backgroundColor: colorFor(level).alpha(0.85).string(),
-                borderBottomLeftRadius: level === DangerLevel.Low ? 4 : 0,
-                borderTopLeftRadius: level === DangerLevel.Low ? 4 : 0,
-                borderBottomRightRadius: level === DangerLevel.Extreme ? 4 : 0,
-                borderTopRightRadius: level === DangerLevel.Extreme ? 4 : 0,
-              }}
-            />
+                backgroundColor: colorFor(level).string(),
+                borderBottomLeftRadius: level === DangerLevel.Low ? 24 : 0,
+                borderTopLeftRadius: level === DangerLevel.Low ? 24 : 0,
+                borderBottomRightRadius: level === DangerLevel.Extreme ? 24 : 0,
+                borderTopRightRadius: level === DangerLevel.Extreme ? 24 : 0,
+              }}>
+              <Text key={level} px="5" color={level < 4 ? 'darkText' : 'white'}>
+                {level}
+              </Text>
+            </Center>
           ))}
-      </View>
-      <View style={styles.legendItems}>
-        {Object.keys(DangerLevel)
-          .filter(key => Number.isNaN(+key))
-          .filter(key => DangerLevel[key] > DangerLevel.None)
-          .map(key => DangerLevel[key])
-          .map(level => (
-            <Text key={level} style={styles.legendText}>
-              {dangerShortText(level)}
-            </Text>
-          ))}
-      </View>
-    </View>
+      </HStack>
+    </HStack>
   );
 };
-
-const styles = StyleSheet.create({
-  legend: {
-    backgroundColor: 'white',
-    padding: 8,
-    borderRadius: 8,
-    display: 'flex',
-    flex: 1,
-    flexDirection: 'column',
-  },
-  legendHeader: {flex: 1, flexDirection: 'row', alignItems: 'center'},
-  legendTitle: {
-    flex: 0,
-    padding: 8,
-    fontWeight: 'bold',
-  },
-  legendIcon: {
-    flex: 0,
-    width: 16,
-    height: 16,
-  },
-  legendItems: {
-    marginLeft: 8,
-    marginRight: 8,
-    display: 'flex',
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-  },
-  legendColor: {
-    flex: 1,
-    height: 16,
-  },
-  legendText: {
-    flex: 1,
-    padding: 2,
-    color: 'black',
-    textAlign: 'center',
-    fontSize: 10,
-  },
-});
