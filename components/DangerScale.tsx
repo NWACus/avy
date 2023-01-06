855fa0137b1afdf455730fb06893f54cab714b7c
Brian Sharon <brian@nwac.us>
2023-01-05 18:58:15 -0800

    Fix dangerscale positioning when running on real device by making it a child of SafeAreaView

--- components/DangerScale.tsx
+++ components/DangerScale.tsx
@@ -4,47 +4,51 @@ import {MaterialIcons} from '@expo/vector-icons';
 
 import {DangerLevel} from 'types/nationalAvalancheCenter';
 import {colorFor} from './AvalancheDangerPyramid';
-import {Center, HStack, useToast} from 'native-base';
+import {Center, HStack, View, useToast} from 'native-base';
 import {BodyXSmBlack, BodyXSmMedium} from 'components/text';
 import {TouchableOpacity} from 'react-native';
 
-export const DangerScale: React.FunctionComponent = () => {
+export type DangerScaleProps = Omit<React.ComponentProps<typeof View>, 'children'>;
+
+export const DangerScale: React.FunctionComponent<DangerScaleProps> = props => {
   const toast = useToast();
 
   return (
-    <HStack backgroundColor="rgba(0, 0, 0, 0.6)" borderRadius={24} px="4" py="2" justifyContent="space-between" alignItems="center">
-      <TouchableOpacity
-        onPress={() => {
-          toast.show({description: 'Not implemented yet'});
-        }}>
-        <HStack space="1" alignItems="center">
-          <BodyXSmBlack color="white" bold style={{letterSpacing: -0.61}}>
-            Danger Scale
-          </BodyXSmBlack>
-          <MaterialIcons name="open-in-new" size={16} color="white" />
+    <View {...props}>
+      <HStack backgroundColor="rgba(0, 0, 0, 0.6)" borderRadius={24} px="4" py="2" justifyContent="space-between" alignItems="center">
+        <TouchableOpacity
+          onPress={() => {
+            toast.show({description: 'Not implemented yet'});
+          }}>
+          <HStack space="1" alignItems="center">
+            <BodyXSmBlack color="white" bold style={{letterSpacing: -0.61}}>
+              Danger Scale
+            </BodyXSmBlack>
+            <MaterialIcons name="open-in-new" size={16} color="white" />
+          </HStack>
+        </TouchableOpacity>
+        <HStack space={0} justifyContent="stretch">
+          {Object.keys(DangerLevel)
+            .filter(key => Number.isNaN(+key))
+            .filter(key => DangerLevel[key] > DangerLevel.None)
+            .map(key => DangerLevel[key])
+            .map(level => (
+              <Center
+                key={level}
+                style={{
+                  backgroundColor: colorFor(level).string(),
+                  borderBottomLeftRadius: level === DangerLevel.Low ? 24 : 0,
+                  borderTopLeftRadius: level === DangerLevel.Low ? 24 : 0,
+                  borderBottomRightRadius: level === DangerLevel.Extreme ? 24 : 0,
+                  borderTopRightRadius: level === DangerLevel.Extreme ? 24 : 0,
+                }}>
+                <BodyXSmMedium px="5" color={level < 4 ? 'darkText' : 'white'}>
+                  {level}
+                </BodyXSmMedium>
+              </Center>
+            ))}
         </HStack>
-      </TouchableOpacity>
-      <HStack space={0} justifyContent="stretch">
-        {Object.keys(DangerLevel)
-          .filter(key => Number.isNaN(+key))
-          .filter(key => DangerLevel[key] > DangerLevel.None)
-          .map(key => DangerLevel[key])
-          .map(level => (
-            <Center
-              key={level}
-              style={{
-                backgroundColor: colorFor(level).string(),
-                borderBottomLeftRadius: level === DangerLevel.Low ? 24 : 0,
-                borderTopLeftRadius: level === DangerLevel.Low ? 24 : 0,
-                borderBottomRightRadius: level === DangerLevel.Extreme ? 24 : 0,
-                borderTopRightRadius: level === DangerLevel.Extreme ? 24 : 0,
-              }}>
-              <BodyXSmMedium px="5" color={level < 4 ? 'darkText' : 'white'}>
-                {level}
-              </BodyXSmMedium>
-            </Center>
-          ))}
       </HStack>
-    </HStack>
+    </View>
   );
 };
