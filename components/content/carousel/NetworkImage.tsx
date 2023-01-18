d2d486f4049a587928eba6c7bec2fe2dcd445bc4
Brian Sharon <brian@nwac.us>
2023-01-18 10:31:59 -0800

    Fix thumbnails for panoramas

--- components/content/carousel/NetworkImage.tsx
+++ components/content/carousel/NetworkImage.tsx
@@ -25,22 +25,30 @@ export const NetworkImage: React.FC<NetworkImageProps> = ({uri, width, height, o
   const [state, setState] = useState<NetworkImageState>('loading');
   const [imageSize, setImageSize] = useState([0, 0]);
 
-  // with this style, the available space is always completely filled. a portrait image is cropped to fit the available space.
-  const croppedThumbnailStyle = {
-    width,
-    height,
-    flex: 1,
-    borderRadius: borderRadius,
-  };
+  // With this style, the available space is always completely filled. a portrait image is cropped to fit the available space.
+  const croppedThumbnailProps = {
+    style: {
+      width,
+      height,
+      flex: 1,
+      borderRadius: borderRadius,
+      ...(typeof imageStyle === 'object' ? imageStyle : {}),
+    },
+    resizeMode: 'cover',
+  } as const;
 
-  // with this style, the full thumbnail is always visible. a portrait image will leave whitespace on the sides.
-  const _fullThumbnailStyle = {
-    width: imageSize[0] === 0 ? undefined : imageSize[0],
-    height: imageSize[1] === 0 ? undefined : imageSize[1],
-    aspectRatio: imageSize[1] > 0 ? imageSize[0] / imageSize[1] : 1,
-    flex: 1,
-    borderRadius: imageSize[1] > imageSize[0] ? 0 : borderRadius, // don't round the corners of a vertical image
-  };
+  // With this style, the full thumbnail is always visible. a portrait image will leave whitespace on the sides.
+  // Note: this style still needs work for panoramic images, it allows bleeding beyond the right edge.
+  const _fullThumbnailProps = {
+    style: {
+      width: imageSize[0] === 0 ? undefined : imageSize[0],
+      height: imageSize[1] === 0 ? undefined : imageSize[1],
+      aspectRatio: imageSize[1] > 0 ? imageSize[0] / imageSize[1] : 1,
+      flex: 1,
+      borderRadius: imageSize[1] > imageSize[0] ? 0 : borderRadius, // don't round the corners of a vertical image
+    },
+    resizeMode: 'contain',
+  } as const;
 
   return (
     <Center width={width} height={height} borderColor={colorLookup('light.200')} borderWidth={1} borderRadius={borderRadius}>
@@ -65,15 +73,7 @@ export const NetworkImage: React.FC<NetworkImageProps> = ({uri, width, height, o
               onStateChange('ready');
             }}
             onError={_e => setState('error')}
-            style={{
-              width: imageSize[0] === 0 ? undefined : imageSize[0],
-              height: imageSize[1] === 0 ? undefined : imageSize[1],
-              aspectRatio: imageSize[1] > 0 ? imageSize[0] / imageSize[1] : 1,
-              flex: 1,
-              ...croppedThumbnailStyle,
-              ...(typeof imageStyle === 'object' ? imageStyle : {}),
-            }}
-            resizeMode="contain"
+            {...croppedThumbnailProps}
           />
         </TouchableOpacity>
       )}
