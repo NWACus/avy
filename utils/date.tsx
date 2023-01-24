b30eebe1fc22e8cbcf6024384c33b78832261216
Steve Kuznetsov <skuznets@redhat.com>
2023-01-24 05:50:01 -0700

    utils: handle UTC conversions with date-fns-tz utility
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- utils/date.tsx
+++ utils/date.tsx
@@ -1,11 +1,13 @@
 import {format} from 'date-fns';
+import {formatInTimeZone} from 'date-fns-tz';
 
 const MISSING_TIMEZONE = /^\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d(\.\d+)?$/;
 
 // Some avalanche.org endpoints return dates without timezone information! This method is used to detect and fix them, by assuming they're UTC.
 export const fixMalformedISO8601DateString = (date: string) => (MISSING_TIMEZONE.test(date) ? `${date}Z` : date);
 
-export const apiDateString = (date: Date) => date.toISOString().substring(0, 10);
+// The National Avalanche Center API expects 'YYYY-MM-DD' date-strings in query parameters, and it operates in UTC.
+export const apiDateString = (date: Date) => formatInTimeZone(date, 'UTC', 'yyyy-MM-dd');
 
 export const utcDateToLocalTimeString = (date: Date | string | undefined): string => {
   if (date == null) {
