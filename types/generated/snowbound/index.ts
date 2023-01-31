stevekuznetsov/avalanche-forecast

Autogenerated types for Snowbound API

generated/snowbound/index.ts
@@ -1,0 +1,26 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export { ApiError } from './core/ApiError';
+export { CancelablePromise, CancelError } from './core/CancelablePromise';
+export { OpenAPI } from './core/OpenAPI';
+export type { OpenAPIConfig } from './core/OpenAPI';
+
+export type { ClientStationMetadata } from './models/ClientStationMetadata';
+export type { Coordinates } from './models/Coordinates';
+export type { DataIngestBase } from './models/DataIngestBase';
+export type { HTTPValidationError } from './models/HTTPValidationError';
+export type { StationMetadata } from './models/StationMetadata';
+export type { StationNote } from './models/StationNote';
+export type { StationTracking } from './models/StationTracking';
+export type { StationTrackingCreate } from './models/StationTrackingCreate';
+export type { ValidationError } from './models/ValidationError';
+export type { VariableTracking } from './models/VariableTracking';
+export type { VariableTrackingCreate } from './models/VariableTrackingCreate';
+
+export { CurrentDataService } from './services/CurrentDataService';
+export { StationMetadataService } from './services/StationMetadataService';
+export { StationNoteService } from './services/StationNoteService';
+export { StationTrackingService } from './services/StationTrackingService';
+export { TimeseriesDataService } from './services/TimeseriesDataService';
+export { VariableTrackingService } from './services/VariableTrackingService';

generated/snowbound/models/StationTracking.ts
@@ -1,0 +1,13 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type StationTracking = {
+  id: string;
+  client_id: number;
+  station_metadata_id: string;
+  stid?: string;
+  source?: string;
+  date_added: string;
+};
+

NWACus/avy

types/generated/snowbound/index.ts
@@ -1,0 +1,26 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export { ApiError } from './core/ApiError';
+export { CancelablePromise, CancelError } from './core/CancelablePromise';
+export { OpenAPI } from './core/OpenAPI';
+export type { OpenAPIConfig } from './core/OpenAPI';
+
+export type { ClientStationMetadata } from './models/ClientStationMetadata';
+export type { Coordinates } from './models/Coordinates';
+export type { DataIngestBase } from './models/DataIngestBase';
+export type { HTTPValidationError } from './models/HTTPValidationError';
+export type { StationMetadata } from './models/StationMetadata';
+export type { StationNote } from './models/StationNote';
+export type { StationTracking } from './models/StationTracking';
+export type { StationTrackingCreateRequest } from './models/StationTrackingCreateRequest';
+export type { ValidationError } from './models/ValidationError';
+export type { VariableTracking } from './models/VariableTracking';
+export type { VariableTrackingCreate } from './models/VariableTrackingCreate';
+
+export { CurrentDataService } from './services/CurrentDataService';
+export { StationMetadataService } from './services/StationMetadataService';
+export { StationNoteService } from './services/StationNoteService';
+export { StationTrackingService } from './services/StationTrackingService';
+export { TimeseriesDataService } from './services/TimeseriesDataService';
+export { VariableTrackingService } from './services/VariableTrackingService';

types/generated/snowbound/models/StationTracking.ts
@@ -1,0 +1,12 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type StationTracking = {
+  id: string;
+  client_id: number;
+  stid: string;
+  source: string;
+  date_added: string;
+};
+