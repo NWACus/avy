38336216aaa2e2e99695767e617aaa5edcc69874
Brian Sharon <brian@nwac.us>
2023-01-31 07:26:16 -0800

    pr feedback

--- scripts/gen-openapi.sh
+++ scripts/gen-openapi.sh
@@ -1,6 +1,18 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
 # Download Snowbounds published API and fix it up locally
-tmpfile=$(mktemp /tmp/openapi.XXXXXX)
-./scripts/getSnowboundOpenAPISpec > $tmpfile
+tmpfile="$(mktemp /tmp/openapi.XXXXXX)"
+
+function cleanup() {
+    rm -rf "${tmpfile}"
+}
+
+trap EXIT cleanup
+
+npx ts-node "scripts/getSnowboundOpenAPISpec.ts" > "$tmpfile"
 
 # Generate TS type definitions from Snowbound's published API
 yarn openapi \
