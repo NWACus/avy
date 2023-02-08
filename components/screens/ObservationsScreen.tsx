e9cc4e19ccdbcd61f4e3cc99394a0b994ed35215
Brian Sharon <brian@nwac.us>
2023-02-08 13:27:30 -0800

    Add new observations screens: portal and submit

--- components/screens/ObservationsScreen.tsx
+++ components/screens/ObservationsScreen.tsx
@@ -4,23 +4,47 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Observations} from 'components/Observations';
 import {Observation} from 'components/Observation';
+import {Center} from 'components/core';
+import {Body} from 'components/text';
 
 const ObservationsStack = createNativeStackNavigator<ObservationsStackParamList>();
 export const ObservationsTabScreen = ({route}: NativeStackScreenProps<TabNavigatorParamList, 'Observations'>) => {
   const {center_id, dateString} = route.params;
   return (
-    <ObservationsStack.Navigator initialRouteName="observations">
-      <ObservationsStack.Screen
-        name="observations"
-        component={ObservationsScreen}
-        initialParams={{center_id: center_id, dateString}}
-        options={() => ({title: `${center_id} Observations`})}
-      />
+    <ObservationsStack.Navigator initialRouteName="observationsPortal" screenOptions={{headerShown: false}}>
+      <ObservationsStack.Screen name="observationsPortal" component={ObservationsPortalScreen} initialParams={{center_id: center_id, dateString}} />
+      <ObservationsStack.Screen name="observationSubmit" component={ObservationSubmitScreen} />
+      <ObservationsStack.Screen name="observationsList" component={ObservationsListScreen} initialParams={{center_id: center_id, dateString}} />
       <ObservationsStack.Screen name="observation" component={ObservationScreen} />
     </ObservationsStack.Navigator>
   );
 };
-const ObservationsScreen = ({route}: NativeStackScreenProps<ObservationsStackParamList, 'observations'>) => {
+
+const ObservationsPortalScreen = ({route}: NativeStackScreenProps<ObservationsStackParamList, 'observationsPortal'>) => {
+  const {center_id, dateString} = route.params;
+  return (
+    <View style={styles.fullScreen}>
+      <Center width="100%" height="100%">
+        <Body>
+          this is the observations portal {center_id} {dateString}
+        </Body>
+      </Center>
+    </View>
+  );
+};
+
+const ObservationSubmitScreen = ({route}: NativeStackScreenProps<ObservationsStackParamList, 'observationSubmit'>) => {
+  const {center_id} = route.params;
+  return (
+    <View style={styles.fullScreen}>
+      <Center width="100%" height="100%">
+        <Body>this is the observation submit screen {center_id}</Body>
+      </Center>
+    </View>
+  );
+};
+
+const ObservationsListScreen = ({route}: NativeStackScreenProps<ObservationsStackParamList, 'observationsList'>) => {
   const {center_id, dateString} = route.params;
   return (
     <View style={styles.fullScreen}>
@@ -28,6 +52,7 @@ const ObservationsScreen = ({route}: NativeStackScreenProps<ObservationsStackPar
     </View>
   );
 };
+
 const ObservationScreen = ({route}: NativeStackScreenProps<ObservationsStackParamList, 'observation'>) => {
   const {id} = route.params;
   return (
