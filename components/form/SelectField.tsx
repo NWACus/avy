0e165ddeaa3a3464df37043086b44704ec22bb75
Brian Sharon <brian@nwac.us>
2023-02-17 14:50:32 -0800

    Add shadow to dropdown

--- components/form/SelectField.tsx
+++ components/form/SelectField.tsx
@@ -53,6 +53,10 @@ const selectStyles: SelectStyles = {
     borderWidth: 2,
     borderColor,
     borderRadius: 4,
+    shadowColor: '#171717',
+    shadowOffset: {width: -2, height: 4},
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
   },
 };
 
