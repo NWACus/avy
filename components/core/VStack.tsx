421e87f15b1f68bf86d619f74c7f20561ea80155
Brian Sharon <brian@nwac.us>
2023-02-23 11:46:23 -0800

    Set up ref forwarding down the Field -> VStack -> View chain

--- components/core/VStack.tsx
+++ components/core/VStack.tsx
@@ -2,7 +2,7 @@ import {merge} from 'lodash';
 import * as React from 'react';
 
 import {View, ViewProps} from 'components/core/View';
-import {ViewStyle} from 'react-native';
+import {View as RNView, ViewStyle} from 'react-native';
 
 const baseStyle: ViewStyle = {
   flexDirection: 'column',
@@ -14,20 +14,22 @@ export interface VStackProps extends ViewProps {
   space?: number;
 }
 
-export const VStack: React.FC<VStackProps> = React.memo(({children: originalChildren, style: originalStyle = {}, space, ...props}) => {
-  const style = {};
-  merge(style, baseStyle, originalStyle);
-  const children = (() => {
-    if (typeof space === 'number') {
-      return React.Children.toArray(originalChildren)
-        .map((child, index) => (index > 0 ? [<View height={space} flex={0} key={`vstack-space-${index}`} />, child] : child))
-        .flat();
-    }
-    return originalChildren;
-  })();
-  return (
-    <View style={style} {...props}>
-      {children}
-    </View>
-  );
-});
+export const VStack = React.memo(
+  React.forwardRef<RNView, VStackProps>(({children: originalChildren, style: originalStyle = {}, space, ...props}, ref) => {
+    const style = {};
+    merge(style, baseStyle, originalStyle);
+    const children = (() => {
+      if (typeof space === 'number') {
+        return React.Children.toArray(originalChildren)
+          .map((child, index) => (index > 0 ? [<View height={space} flex={0} key={`vstack-space-${index}`} />, child] : child))
+          .flat();
+      }
+      return originalChildren;
+    })();
+    return (
+      <View style={style} {...props} ref={ref}>
+        {children}
+      </View>
+    );
+  }),
+);
