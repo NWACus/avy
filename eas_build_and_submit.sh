7f0cb13e487fbada930791806952cb28c90c24d7
Yulia Dubinina <yuliadub@users.noreply.github.com>
2023-02-27 17:29:21 -0800

    adjust profile to be the branch name

--- eas_build_and_submit.sh
+++ eas_build_and_submit.sh
@@ -4,4 +4,4 @@ sed -i 's/IOS_USER_ID/${IOS_USER_ID}/g' eas.json
 sed -i 's/IOS_TEAM_ID/${IOS_TEAM_ID}/g' eas.json
 sed -i 's/IOS_APP_ID/${IOS_APP_ID}/g' eas.json
 
-eas build --non-interactive --platform all --profile ${PROFILE} --auto-submit
+eas build --non-interactive --platform all --profile ${GITHUB_REF##*/} --auto-submit
