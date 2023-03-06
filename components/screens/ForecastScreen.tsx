eb1c59f6156ea01f34ce90b854065085dbf6ab9c
Brian Sharon <brian@nwac.us>
2023-03-06 15:42:01 -0800

    forecast screen: header bg white

--- components/screens/ForecastScreen.tsx
+++ components/screens/ForecastScreen.tsx
@@ -12,7 +12,7 @@ export const ForecastScreen = ({route}: NativeStackScreenProps<HomeStackParamLis
   const {center_id, forecast_zone_id, requestedTime, zoneName} = route.params;
   return (
     // hat tip to https://github.com/react-navigation/react-navigation/issues/8694 for the use of `edges`
-    <SafeAreaView style={StyleSheet.absoluteFillObject} edges={['top', 'left', 'right']}>
+    <SafeAreaView style={{...StyleSheet.absoluteFillObject, backgroundColor: 'white'}} edges={['top', 'left', 'right']}>
       <AvalancheForecast center_id={center_id} forecast_zone_id={forecast_zone_id} requestedTime={parseRequestedTimeString(requestedTime)} zoneName={zoneName} />
     </SafeAreaView>
   );
