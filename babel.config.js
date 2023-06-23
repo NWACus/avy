b8da3e01dd4dca2b623a06cf7aac6f89b2514715
Steve Kuznetsov <skuznets@redhat.com>
2023-06-23 12:52:30 -0600

    *: add tamagui to the project
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,24 @@
+process.env.TAMAGUI_TARGET = 'native';
+
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      [
+        'transform-inline-environment-variables',
+        {
+          include: 'TAMAGUI_TARGET',
+        },
+      ],
+      [
+        '@tamagui/babel-plugin',
+        {
+          components: ['tamagui'],
+          config: './tamagui.config.ts',
+          logTimings: true,
+        },
+      ],
       [
         'module-resolver',
         {
