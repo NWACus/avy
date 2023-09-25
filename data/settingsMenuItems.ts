f35c53a6de01a4f4e5937f2ca8d7e0cd5773d0c6
Brian Sharon <brian@nwac.us>
2023-09-25 09:13:18 -0600

    Update NWAC text and links in About page

--- data/settingsMenuItems.ts
+++ data/settingsMenuItems.ts
@@ -2,27 +2,23 @@ export const settingsMenuItems: Record<string, {title: string; url: string}[]> =
   NWAC: [
     {
       title: 'About NWAC',
-      url: 'http://www.nwac.us/about/about-us',
+      url: 'http://nwac.us/about/about-us',
     },
     {
       title: 'Avalanche Education (Learn)',
-      url: 'http://www.nwac.us/about/about-us',
+      url: 'https://nwac.us/avalanche-education/',
     },
     {
-      title: 'Become a Member',
-      url: 'http://www.nwac.us/about/about-us',
+      title: 'Become A Member',
+      url: 'https://nwac.us/membership/',
     },
     {
-      title: 'Volunteer with NWAC',
-      url: 'http://www.nwac.us/about/about-us',
-    },
-    {
-      title: 'Support NWAC',
-      url: 'http://www.nwac.us/about/about-us',
+      title: 'Volunteer With NWAC',
+      url: 'https://support.nwac.us/volunteer/',
     },
     {
       title: 'Latest News',
-      url: 'http://www.nwac.us/about/about-us',
+      url: 'http://nwac.us/#news',
     },
   ],
   SNFAC: [
