e260db25afe3e9003dc08277768071def1d836f1
Brian Sharon <brian@nwac.us>
2023-09-25 08:16:00 -0600

    Use the locally installed version of ESLint

--- scripts/eslint.sh
+++ scripts/eslint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "🧹 Running ESLint..."
-if ! eslint --report-unused-disable-directives --max-warnings=0 .; then
+if ! yarn eslint --report-unused-disable-directives --max-warnings=0 .; then
     echo
     echo "⚠️  ESLint failed"
     echo
