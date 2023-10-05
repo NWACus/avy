e72eab2264bf250bef5fc66423f4bcc06c0036e9
Brian Sharon <brian@nwac.us>
2023-10-05 10:59:34 -0600

    Getting work queue stood up and functional

--- components/observations/ObservationFormData.ts
+++ components/observations/ObservationFormData.ts
@@ -37,82 +37,85 @@ const locationPointSchema = z.object(
 );
 export type LocationPoint = z.infer<typeof locationPointSchema>;
 
-// For the form, we have specific rules that we require for any new observations
-// we create, thus we don't reuse the existing observationSchema
-export const simpleObservationFormSchema = z
-  .object({
-    activity: z.array(z.nativeEnum(Activity)).min(1, 'You must select at least one activity.'),
-    avalanches_summary: z.string().optional(),
-    email: z.string({required_error: required}).email("That doesn't look like an email address."),
-    instability: z.object({
-      avalanches_observed: z.boolean(),
-      avalanches_triggered: z.boolean(),
-      avalanches_caught: z.boolean(),
-      cracking: z.boolean(),
-      cracking_description: z.nativeEnum(InstabilityDistribution).optional(),
-      collapsing: z.boolean(),
-      collapsing_description: z.nativeEnum(InstabilityDistribution).optional(),
-    }),
-    location_name: z.string({required_error: required}).max(256, tooLong),
-    location_point: locationPointSchema,
-    name: z.string({required_error: required}).max(50, tooLong),
-    observation_summary: z.string({required_error: required}).max(1024, tooLong),
-    photoUsage: z.nativeEnum(MediaUsage, {required_error: required}),
-    private: z.boolean(),
-    start_date: z.date({required_error: required}),
-  })
-  .superRefine((arg, ctx) => {
-    // Some more complex validations to apply here
+// This schema captures all of the data fields that we upload in order to create a new observation.
+// This schema can be used to persist an observation to disk and later parse it, for resuming uploads when we're offline.
+export const simpleObservationFormSchema = z.object({
+  activity: z.array(z.nativeEnum(Activity)).min(1, 'You must select at least one activity.'),
+  avalanches_summary: z.string().optional(),
+  email: z.string({required_error: required}).email("That doesn't look like an email address."),
+  instability: z.object({
+    avalanches_observed: z.boolean(),
+    avalanches_triggered: z.boolean(),
+    avalanches_caught: z.boolean(),
+    cracking: z.boolean(),
+    cracking_description: z.nativeEnum(InstabilityDistribution).optional(),
+    collapsing: z.boolean(),
+    collapsing_description: z.nativeEnum(InstabilityDistribution).optional(),
+  }),
+  location_name: z.string({required_error: required}).max(256, tooLong),
+  location_point: locationPointSchema,
+  name: z.string({required_error: required}).max(50, tooLong),
+  observation_summary: z.string({required_error: required}).max(1024, tooLong),
+  photoUsage: z.nativeEnum(MediaUsage, {required_error: required}),
+  private: z.boolean(),
+  start_date: z.date({required_error: required}),
+});
 
-    // if instability.avalanches_observed, then we'll want avalanches_summary to exist
-    if (arg.instability?.avalanches_observed) {
-      const {avalanches_summary} = arg;
-      if (!avalanches_summary || avalanches_summary.length === 0) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message: required,
-          path: ['avalanches_summary'],
-        });
-      } else if (avalanches_summary.length > 1024) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message: tooLong,
-          path: ['avalanches_summary'],
-        });
-      }
+// For the form, we have specific validation rules that we require for any new observations
+// we create, rules that can't be captured via simpleObservationFormSchema. Note that the result of `superRefine`
+// can't be converted to a TS type via infer. This is only used for form validation.
+export const simpleObservationFormSchemaWithValidations = simpleObservationFormSchema.superRefine((arg, ctx) => {
+  // Some more complex validations to apply here
+
+  // if instability.avalanches_observed, then we'll want avalanches_summary to exist
+  if (arg.instability?.avalanches_observed) {
+    const {avalanches_summary} = arg;
+    if (!avalanches_summary || avalanches_summary.length === 0) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: required,
+        path: ['avalanches_summary'],
+      });
+    } else if (avalanches_summary.length > 1024) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: tooLong,
+        path: ['avalanches_summary'],
+      });
     }
+  }
 
-    // if instability.cracking, then we'll want cracking_description to be set
-    // cracking_description is an enum, so no max length validation is required -
-    // it just needs to be set
-    if (arg.instability?.cracking) {
-      const {cracking_description} = arg.instability;
-      if (!cracking_description || cracking_description.length === 0) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message: required,
-          path: ['instability', 'cracking_description'],
-        });
-      }
+  // if instability.cracking, then we'll want cracking_description to be set
+  // cracking_description is an enum, so no max length validation is required -
+  // it just needs to be set
+  if (arg.instability?.cracking) {
+    const {cracking_description} = arg.instability;
+    if (!cracking_description || cracking_description.length === 0) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: required,
+        path: ['instability', 'cracking_description'],
+      });
     }
+  }
 
-    // if instability.collapsing, then we'll want collapsing_description to be set
-    // collapsing_description is an enum, so no max length validation is required -
-    // it just needs to be set
-    if (arg.instability?.collapsing) {
-      const {collapsing_description} = arg.instability;
-      if (!collapsing_description || collapsing_description.length === 0) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message: required,
-          path: ['instability', 'collapsing_description'],
-        });
-      }
+  // if instability.collapsing, then we'll want collapsing_description to be set
+  // collapsing_description is an enum, so no max length validation is required -
+  // it just needs to be set
+  if (arg.instability?.collapsing) {
+    const {collapsing_description} = arg.instability;
+    if (!collapsing_description || collapsing_description.length === 0) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: required,
+        path: ['instability', 'collapsing_description'],
+      });
     }
+  }
 
-    return z.NEVER;
-  });
+  return z.NEVER;
+});
 
-export interface ObservationFormData extends z.infer<typeof simpleObservationFormSchema> {
+export interface ObservationFormData extends z.infer<typeof simpleObservationFormSchemaWithValidations> {
   images: ImagePickerAsset[];
 }
--- components/observations/submitObservation.ts
+++ components/observations/submitObservation.ts
@@ -1,14 +1,103 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
-import {Logger} from 'browser-bunyan';
 import * as FileSystem from 'expo-file-system';
 import {manipulateAsync, SaveFormat} from 'expo-image-manipulator';
-import {ImagePickerAsset} from 'expo-image-picker';
-import md5 from 'md5';
+import uuid from 'react-native-uuid';
+import {z} from 'zod';
 
-import {ObservationFormData} from 'components/observations/ObservationFormData';
-import {AvalancheCenterID, MediaItem, mediaItemSchema, MediaUsage, Observation} from 'types/nationalAvalancheCenter';
+import {ObservationFormData, simpleObservationFormSchema} from 'components/observations/ObservationFormData';
+import {logger} from 'logger';
+import {AvalancheCenterID, avalancheCenterIDSchema, MediaItem, mediaItemSchema, MediaUsage, Observation} from 'types/nationalAvalancheCenter';
 import {apiDateString} from 'utils/date';
+
+const taskQueueEntryTypeSchema = z.enum(['image', 'observation']);
+type TaskQueueEntryType = z.infer<typeof taskQueueEntryTypeSchema>;
+
+const taskQueueEntrySchema = z.discriminatedUnion('type', [
+  z.object({
+    id: z.string().uuid(),
+    type: z.literal('image'),
+    data: z.object({
+      apiPrefix: z.string(),
+      image: z.object({
+        uri: z.string(),
+        exif: z
+          .object({
+            Orientation: z.number().or(z.string()).optional(),
+          })
+          .optional(),
+      }),
+      name: z.string(),
+      center_id: avalancheCenterIDSchema,
+      photoUsage: z.nativeEnum(MediaUsage),
+    }),
+  }),
+  z.object({
+    id: z.string().uuid(),
+    type: z.literal('observation'),
+    data: simpleObservationFormSchema.partial().extend({
+      url: z.string().url(),
+      imageTaskIds: z.array(z.string().uuid()),
+      center_id: avalancheCenterIDSchema,
+      organization: avalancheCenterIDSchema,
+      status: z.literal('published'),
+      observer_type: z.literal('public'),
+      // Date has to be a plain-old YYYY-MM-DD string
+      start_date: z.string(),
+    }),
+  }),
+]);
+type TaskQueueEntry = z.infer<typeof taskQueueEntrySchema>;
+type ImageTaskData = Extract<TaskQueueEntry, {type: 'image'}>['data'];
+type ObservationTaskData = Extract<TaskQueueEntry, {type: 'observation'}>['data'];
+
+const taskQueueSchema = z.array(taskQueueEntrySchema);
+
+const TASK_QUEUE_KEY = 'TASK_QUEUE';
+let pendingTaskQueueUpdate: null | NodeJS.Timeout = null;
+
+const processTaskQueue = async () => {
+  pendingTaskQueueUpdate = null;
+  const queue = taskQueueSchema.parse(JSON.parse((await AsyncStorage.getItem(TASK_QUEUE_KEY)) || '[]'));
+  logger.debug({queue}, 'processTaskQueue');
+  if (queue.length === 0) {
+    return;
+  }
+  const entry = queue[0];
+  try {
+    switch (entry.type) {
+      case 'image':
+        await uploadImage(entry.id, entry.data);
+        break;
+      case 'observation':
+        await uploadObservation(entry.id, entry.data);
+        break;
+    }
+    logger.debug({entry}, `processed task queue entry successfully`);
+    await AsyncStorage.setItem(TASK_QUEUE_KEY, JSON.stringify(queue.slice(1)));
+    subscribers.forEach(subscriber => subscriber(entry, true, 0));
+  } catch (error) {
+    logger.error({error, entry}, `error processing task queue entry`);
+    subscribers.forEach(subscriber => subscriber(entry, false, 0));
+  } finally {
+    pendingTaskQueueUpdate = setTimeout(() => void processTaskQueue(), 1000);
+  }
+};
+
+const enqueueTasks = async (entries: TaskQueueEntry[]) => {
+  logger.debug({entries}, 'enqueueTasks');
+  const queue = taskQueueSchema.parse(JSON.parse((await AsyncStorage.getItem(TASK_QUEUE_KEY)) || '[]'));
+  queue.push(...entries);
+  await AsyncStorage.setItem(TASK_QUEUE_KEY, JSON.stringify(queue));
+  if (!pendingTaskQueueUpdate) {
+    pendingTaskQueueUpdate = setTimeout(() => void processTaskQueue(), 1000);
+  }
+};
+
+const resetTaskQueue = async () => {
+  logger.debug('resetTaskQueue');
+  await AsyncStorage.setItem(TASK_QUEUE_KEY, JSON.stringify([]));
+};
 
 const extensionToMimeType = (extension: string) => {
   switch (extension.toLowerCase()) {
@@ -30,22 +119,53 @@ export const clearUploadCache = async () => {
   keys.filter(k => k.startsWith(imageUploadCachePrefix)).map(async k => await AsyncStorage.removeItem(k));
 };
 
+const getUploadedImageByTaskId = async (taskId: string): Promise<MediaItem | null> => {
+  const imageCacheKey = `${imageUploadCachePrefix}:${taskId}`;
+  const imageCacheString = await AsyncStorage.getItem(imageCacheKey);
+  return imageCacheString ? mediaItemSchema.parse(JSON.parse(imageCacheString)) : null;
+};
+
+const setUploadedImageByTaskId = async (taskId: string, image: MediaItem): Promise<void> => {
+  const imageCacheKey = `${imageUploadCachePrefix}:${taskId}`;
+  await AsyncStorage.setItem(imageCacheKey, JSON.stringify(image));
+};
+
+const clearUploadedImageByTaskId = async (taskId: string): Promise<void> => {
+  const imageCacheKey = `${imageUploadCachePrefix}:${taskId}`;
+  await AsyncStorage.removeItem(imageCacheKey);
+};
+
+type Subscriber = (entry: TaskQueueEntry, success: boolean, attempts: number) => void;
+let subscribers: Subscriber[] = [];
+const subscribeToTaskInvocations = (callback: Subscriber) => {
+  subscribers.push(callback);
+};
+
+const unsubscribeFromTaskInvocations = (callback: Subscriber) => {
+  subscribers = subscribers.filter(subscriber => subscriber !== callback);
+};
+
+interface PickedImage {
+  uri: string;
+  exif?: {
+    // this is how it's defined in expo-image-picker
+    Orientation?: string | number;
+  };
+}
 interface UploadImageOptions {
   apiPrefix: string;
   center_id: AvalancheCenterID;
-  image: ImagePickerAsset;
+  image: PickedImage;
   name: string;
   photoUsage: MediaUsage;
 }
 
-const getImageData = async (image: ImagePickerAsset): Promise<{imageDataBase64: string; mimeType: string; filename: string}> => {
-  const {uri} = image;
-
+const loadImageData = async ({uri, exif}: PickedImage): Promise<{imageDataBase64: string; mimeType: string; filename: string}> => {
   // This weird use of `slice` is because the version of Hermes that Expo is currently pinned to doesn't support `at()`
   const filename = uri.split('/').slice(-1)[0];
   const extension = filename.split('.').slice(-1)[0] || '';
 
-  const orientation = image.exif?.Orientation as string | number;
+  const orientation = exif?.Orientation as string | number;
   if (typeof orientation !== 'number' || orientation <= 1) {
     const imageDataBase64 = await FileSystem.readAsStringAsync(uri, {encoding: 'base64'});
     return {imageDataBase64, filename, mimeType: extensionToMimeType(extension)};
@@ -60,8 +180,22 @@ const getImageData = async (image: ImagePickerAsset): Promise<{imageDataBase64: 
   }
 };
 
-const uploadImage = async (logger: Logger, {apiPrefix, image, name, center_id, photoUsage}: UploadImageOptions): Promise<MediaItem> => {
-  const {imageDataBase64, filename, mimeType} = await getImageData(image);
+const uploadImage = async (taskId: string, {apiPrefix, image, name, center_id, photoUsage}: UploadImageOptions): Promise<MediaItem> => {
+  // If we've already uploaded this image once, don't do it again.
+  const cached = await getUploadedImageByTaskId(taskId);
+  const thisLogger = logger.child({uri: image.uri});
+  if (cached) {
+    thisLogger.debug({taskId, image}, `image has already been uploaded, using cached media item`);
+    try {
+      return Promise.resolve(cached);
+    } catch (error) {
+      thisLogger.warn(`unable to load cached image data, uploading it again`);
+      await clearUploadedImageByTaskId(taskId);
+      // fallthrough
+    }
+  }
+
+  const {imageDataBase64, filename, mimeType} = await loadImageData(image);
 
   const payload = {
     file: `data:${mimeType};base64,${imageDataBase64}`,
@@ -75,74 +209,127 @@ const uploadImage = async (logger: Logger, {apiPrefix, image, name, center_id, p
     // TODO would be nice to tag images that came from this app, but haven't figured that out yet
   };
 
-  // If we've already uploaded this image once with a particular set of settings, don't do it again.
-  const payloadHash = md5(JSON.stringify(payload));
-  const imageCacheKey = `${imageUploadCachePrefix}:${payloadHash}`;
-  const cached = await AsyncStorage.getItem(imageCacheKey);
-  const thisLogger = logger.child({uri: image.uri});
-  if (cached) {
-    thisLogger.debug({payload: payload}, `image has already been uploaded, using cached media item`);
-    try {
-      return Promise.resolve(mediaItemSchema.parse(JSON.parse(cached)));
-    } catch (error) {
-      thisLogger.warn(`unable to load cached image data, uploading it again`);
-      await AsyncStorage.removeItem(imageCacheKey);
-      // fallthrough
-    }
-  }
-
   const response = await axios.post<MediaItem>(`${apiPrefix}/v2/public/media`, payload, {
     headers: {
       // Public API uses the Origin header to determine who's authorized to call it
       Origin: 'https://nwac.us',
     },
   });
-  await AsyncStorage.setItem(imageCacheKey, JSON.stringify(response.data));
+  await setUploadedImageByTaskId(taskId, response.data);
   return response.data;
 };
 
-export const submitObservation = async (
-  logger: Logger,
-  {
-    apiPrefix,
-    center_id,
-    observationFormData,
-  }: {
-    apiPrefix: string;
-    center_id: AvalancheCenterID;
-    observationFormData: Partial<ObservationFormData>;
-  },
-): Promise<Observation> => {
-  const {photoUsage, name} = observationFormData;
-  // TODO: probably should upload these sequentially instead of in parallel
-  const media = await Promise.all(
-    observationFormData.images?.map(image =>
-      uploadImage(logger, {
-        apiPrefix: apiPrefix,
-        image: image,
-        name: name ?? '',
-        center_id: center_id,
-        photoUsage: photoUsage ?? MediaUsage.Credit,
-      }),
-    ) ?? [],
-  );
-  logger.info({media: media}, 'submitted media');
-
-  const url = `${apiPrefix}/obs/v1/public/observation/`;
-  const payload = {
-    ...observationFormData,
-    center_id,
-    organization: center_id,
-    status: 'published',
-    media: media,
-    observer_type: 'public',
-    // Date has to be a plain-old YYYY-MM-DD string
-    start_date: apiDateString(observationFormData.start_date ?? new Date()),
+async function uploadObservation(id: string, data: ObservationTaskData): Promise<Observation> {
+  const {url, imageTaskIds, ...params} = data;
+  const media = (await Promise.all(imageTaskIds.map(getUploadedImageByTaskId))).filter((image): image is MediaItem => image != null);
+  const payload: Partial<Observation> = {
+    ...params,
+    media,
+    obs_source: 'public',
+    body: params.observation_summary,
   };
+  try {
+    const {data: responseData} = await axios.post<Observation>(url, payload, {
+      headers: {
+        // Public API uses the Origin header to determine who's authorized to call it
+        Origin: 'https://nwac.us',
+      },
+    });
+    // You'd think we could feed data to Zod and get a strongly typed object back, but
+    // the object that we get back from the post can't actually be parsed by our schema :(
+    // TODO(skuznets): figure out what we get from POST and actually parse it ...
+    return responseData;
+  } catch (error) {
+    logger.error(
+      {
+        error,
+        url,
+        payload,
+      },
+      'error uploading observation',
+    );
+    throw error;
+  }
+}
 
-  const {data} = await axios.post<Observation>(url, payload);
-  // You'd think we could feed data to Zod and get a strongly typed object back, but
-  // the object that we get back from the post can't actually be parsed by our schema :(
-  // TODO(skuznets): figure out what we get from POST and actually parse it ...
-  return data;
+export const submitObservation = async ({
+  apiPrefix,
+  center_id,
+  observationFormData,
+}: {
+  apiPrefix: string;
+  center_id: AvalancheCenterID;
+  observationFormData: Partial<ObservationFormData>;
+}): Promise<void> => {
+  try {
+    const {photoUsage, name} = observationFormData;
+
+    const tasks: TaskQueueEntry[] = [];
+
+    observationFormData.images?.forEach(image => {
+      tasks.push({
+        // uuid.v4 has a goofy implementation that only returns a byte array if you pass a byte array in
+        id: uuid.v4() as string,
+        type: 'image',
+        data: {
+          apiPrefix: apiPrefix,
+          image: {
+            uri: image.uri,
+            exif: image.exif
+              ? {
+                  // The type of ImagePickerAsset.exif is (unfortunately) Record<string, any>
+                  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                  Orientation: image.exif.Orientation,
+                }
+              : undefined,
+          },
+          name: name ?? '',
+          center_id: center_id,
+          photoUsage: photoUsage ?? MediaUsage.Credit,
+        },
+      });
+    });
+
+    const imageTaskIds = tasks.map(task => task.id);
+    // uuid.v4 has a goofy implementation that only returns a byte array if you pass a byte array in
+    const uploadTaskId = uuid.v4() as string;
+    const url = `${apiPrefix}/obs/v1/public/observation/`;
+    tasks.push({
+      id: uploadTaskId,
+      type: 'observation',
+      data: {
+        ...observationFormData,
+        imageTaskIds,
+        url,
+        center_id,
+        organization: center_id,
+        status: 'published',
+        observer_type: 'public',
+        // Date has to be a plain-old YYYY-MM-DD string
+        start_date: apiDateString(observationFormData.start_date ?? new Date()),
+      },
+    });
+
+    const promise: Promise<void> = new Promise((resolve, _reject) => {
+      const listener = (entry: TaskQueueEntry, success: boolean) => {
+        if (entry.type === 'observation' && entry.id === uploadTaskId) {
+          if (success) {
+            resolve(/*entry.data as Observation*/);
+            unsubscribeFromTaskInvocations(listener);
+          }
+        }
+      };
+      subscribeToTaskInvocations(listener);
+    });
+
+    await enqueueTasks(tasks);
+
+    return promise;
+  } catch (e) {
+    logger.error({e}, 'error submitting observation');
+    throw e;
+  }
 };
+
+// Kick off the task queue on startup
+pendingTaskQueueUpdate = setTimeout(() => void processTaskQueue(), 1000);
