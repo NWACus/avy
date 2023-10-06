ade3296df1d54830e1ee77366c00ded49ebecb8e
Brian Sharon <brian@nwac.us>
2023-10-06 09:55:23 -0600

    Fix logging of image data in ObservationUploader

--- logger.ts
+++ logger.ts
@@ -41,3 +41,21 @@ export const logger = createLogger({
   serializers: stdSerializers,
   streams,
 });
+
+// Sometimes we're sending very large data params like base64-encoded images.
+// Let's not log those, eh?
+export const filterBigStrings = (params: unknown): unknown => {
+  if (!params || (typeof params !== 'object' && typeof params !== 'string')) {
+    return params;
+  }
+
+  if (typeof params === 'string') {
+    return params.length > 100 ? params.substring(0, 100) + '...' : params;
+  }
+
+  if (Array.isArray(params)) {
+    return params.map(filterBigStrings);
+  }
+
+  return Object.fromEntries(Object.entries(params).map(([k, v]) => [k, filterBigStrings(v)]));
+};
