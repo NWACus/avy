06252e2b3cc2368a0f4701566b1cbb70ac4e71b3
Brian Sharon <brian@nwac.us>
2023-10-31 15:40:02 -0700

    Dial back drop shadow strength

--- components/content/Card.tsx
+++ components/content/Card.tsx
@@ -22,12 +22,12 @@ const pressableStyle: ViewStyle = {
   shadowColor: '#000',
   shadowOffset: {
     width: 0,
-    height: 2,
+    height: 1,
   },
-  shadowOpacity: 0.23,
-  shadowRadius: 2.62,
+  shadowOpacity: 0.2,
+  shadowRadius: 1.41,
 
-  elevation: 4,
+  elevation: 2,
 } as const;
 
 export const Card: React.FunctionComponent<PropsWithChildren<CardProps>> = ({
