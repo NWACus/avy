a87d811b28593f3c10849e3af1d2a96d2bce855f
Steve Kuznetsov <skuznets@redhat.com>
2023-11-29 09:06:18 -0700

    avalancheCenters: only fetch the metadata we need
    We're finding that we can't trust avalanche center metadata to not be
    broken, so we need to make sure that only supported centers are loaded
    and we don't run into issues with failures loading centers we're not
    going to display.
    
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- hooks/useAllAvalancheCenterMetadata.ts
+++ hooks/useAllAvalancheCenterMetadata.ts
@@ -4,13 +4,13 @@ import {useQueries, useQueryClient, UseQueryOptions} from '@tanstack/react-query
 import {AxiosError} from 'axios';
 
 import {ClientContext, ClientProps} from 'clientContext';
-import {filterToKnownCenters} from 'components/avalancheCenterList';
+import {AvalancheCenters, filterToKnownCenters, filterToSupportedCenters} from 'components/avalancheCenterList';
 import AvalancheCenterMetadataQuery from 'hooks/useAvalancheCenterMetadata';
 import {LoggerContext, LoggerProps} from 'loggerContext';
 import {AllAvalancheCenterCapabilities, AvalancheCenter, AvalancheCenterID} from 'types/nationalAvalancheCenter';
 import {ZodError} from 'zod';
 
-export const useAllAvalancheCenterMetadata = (capabilities: AllAvalancheCenterCapabilities | undefined) => {
+export const useAllAvalancheCenterMetadata = (capabilities: AllAvalancheCenterCapabilities | undefined, whichCenters: AvalancheCenters) => {
   const {nationalAvalancheCenterHost} = React.useContext<ClientProps>(ClientContext);
   const {logger} = React.useContext<LoggerProps>(LoggerContext);
   const queryClient = useQueryClient();
@@ -20,9 +20,18 @@ export const useAllAvalancheCenterMetadata = (capabilities: AllAvalancheCenterCa
     knownCenters.push(...filterToKnownCenters(capabilities.centers.map(center => center.id)));
   }
 
+  const filteredCenters: AvalancheCenterID[] = [];
+  switch (whichCenters) {
+    case AvalancheCenters.AllCenters:
+      filteredCenters.push(...knownCenters);
+      break;
+    case AvalancheCenters.SupportedCenters:
+      filteredCenters.push(...filterToSupportedCenters(knownCenters));
+  }
+
   return useQueries<UseQueryOptions<AvalancheCenter, AxiosError | ZodError>[]>({
-    queries: knownCenters
-      ? knownCenters.map(center => {
+    queries: filteredCenters
+      ? filteredCenters.map(center => {
           return {
             queryKey: AvalancheCenterMetadataQuery.queryKey(nationalAvalancheCenterHost, center),
             queryFn: async (): Promise<AvalancheCenter> => AvalancheCenterMetadataQuery.fetchQuery(queryClient, nationalAvalancheCenterHost, center, logger),
