1dc4e874ad7c06eb406bb69c7c98844a772a2c8b
Brian Sharon <brian@nwac.us>
2023-11-29 22:40:55 -0800

    PR feedback

--- components/observations/uploader/uploadObservation.ts
+++ components/observations/uploader/uploadObservation.ts
@@ -1,24 +1,20 @@
 import axios from 'axios';
+import {format} from 'date-fns';
 
 import {ObservationTaskData} from 'components/observations/uploader/Task';
 import {logger} from 'logger';
 import {Observation} from 'types/nationalAvalancheCenter';
-import {apiDateString} from 'utils/date';
 
 export async function uploadObservation(id: string, data: ObservationTaskData): Promise<Observation> {
   const {formData, extraData} = data;
   const {url, ...params} = extraData;
-  // The user only picks a date, not a time. The time defaults to whatever the current time is in the user's timezone.
-  // If the time is late enough, it might be in the next day when converted to UTC. By setting the time to noon in the user's
-  // local timezone, we mitigate this problem.
-  const adjustedDate = new Date(formData.start_date);
-  adjustedDate.setHours(12, 0, 0, 0);
   const payload: Partial<Observation> = {
     ...formData,
     ...params,
     obs_source: 'public',
-    // Date has to be a plain-old YYYY-MM-DD string
-    start_date: apiDateString(adjustedDate),
+    // Date has to be a plain-old YYYY-MM-DD string. This format is the same format used by
+    // `apiDateString`, but that function also converts to UTC, which we don't want to do here (#584)
+    start_date: format(formData.start_date, 'yyyy-MM-dd'),
     status: 'published',
   };
   try {
