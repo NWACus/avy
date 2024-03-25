b722a919a1bfb7361cacf1a567d6da7def7e4cd7
Beau Collins <beau@collins.pub>
2024-03-25 10:53:19 -0700

    Improve <TextField /> usability
     - Allow setting a height on the container
     - Bordered container is now a hit area to focus the field
     - Track focus state and change border color

--- components/form/TextField.tsx
+++ components/form/TextField.tsx
@@ -1,8 +1,8 @@
 import {HStack, View, ViewProps, VStack} from 'components/core';
 import {BodySm, BodySmBlack, BodyXSm} from 'components/text';
-import React, {useCallback} from 'react';
+import React, {useCallback, useLayoutEffect, useRef, useState} from 'react';
 import {useController} from 'react-hook-form';
-import {View as RNView, TextInput, TextInputProps} from 'react-native';
+import {Pressable, View as RNView, StyleSheet, TextInput, TextInputProps} from 'react-native';
 import {colorLookup} from 'theme';
 
 export type KeysMatching<T, V> = {[K in keyof T]-?: T[K] extends V ? K : never}[keyof T];
@@ -36,26 +36,69 @@ export const TextField = React.forwardRef<RNView, TextFieldProps>(({name, label,
     [textTransform, field],
   );
 
+  const onBlur = useCallback(() => {
+    setFocused(false);
+    field.onBlur();
+  }, [field]);
+
+  const onFocus = useCallback(() => {
+    setFocused(true);
+  }, []);
+
+  const textFieldRef = useRef<TextInput>(null);
+  const [isFocused, setFocused] = useState(() => textFieldRef?.current?.isFocused ?? false);
+
+  useLayoutEffect(() => {
+    setFocused(textFieldRef?.current?.isFocused ?? false);
+  }, []);
+
+  const handleFocusField = useCallback(() => {
+    textFieldRef.current?.focus();
+  }, []);
+
   return (
     <VStack width="100%" space={4} {...props} ref={ref}>
       <HStack space={4}>
         <BodySmBlack>{label}</BodySmBlack>
         {comment && <BodySm>{comment}</BodySm>}
       </HStack>
-      <View p={8} borderWidth={2} borderColor={colorLookup('border.base')} borderRadius={4}>
-        <TextInput
-          onBlur={field.onBlur}
-          onChangeText={onChangeText}
-          value={field.value as string} // TODO(skuznets): determine why the generics here don't collapse to string itself ...
-          style={textInputDefaultStyle}
-          placeholderTextColor={colorLookup('text.tertiary')}
-          editable={!disabled}
-          {...textInputProps}
-        />
-      </View>
+      <Pressable style={styles.pressContainer} onPress={handleFocusField}>
+        <View
+          p={8}
+          flexBasis={'auto'}
+          flexGrow={1}
+          flexShrink={0}
+          display="flex"
+          borderWidth={2}
+          borderColor={isFocused ? colorLookup('border.active') : colorLookup('border.base')}
+          borderRadius={4}>
+          <TextInput
+            ref={textFieldRef}
+            onBlur={onBlur}
+            onFocus={onFocus}
+            onChangeText={onChangeText}
+            value={field.value as string} // TODO(skuznets): determine why the generics here don't collapse to string itself ...
+            style={textInputDefaultStyle}
+            placeholderTextColor={colorLookup('text.tertiary')}
+            editable={!disabled}
+            {...textInputProps}
+          />
+        </View>
+      </Pressable>
       {/* TODO: animate the appearance/disappearance of the error string */}
       {fieldState.error && <BodyXSm color={colorLookup('error.900')}>{fieldState.error.message}</BodyXSm>}
     </VStack>
   );
 });
 TextField.displayName = 'TextField';
+
+const styles = StyleSheet.create({
+  pressContainer: {
+    flexGrow: 1,
+    flexShrink: 0,
+    flexBasis: 'auto',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'stretch',
+  },
+});
