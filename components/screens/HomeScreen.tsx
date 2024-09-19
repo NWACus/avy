3e86e897bed3f37e5eb54c87f4041fdae35273b8
Kevin Hernandez <kevint.herdez@local.mac>
2024-09-19 11:58:54 -0700

    Issue 693: Fix Submit button not responding

--- components/screens/HomeScreen.tsx
+++ components/screens/HomeScreen.tsx
@@ -1,34 +1,34 @@
-import {createNativeStackNavigator, NativeStackScreenProps} from '@react-navigation/native-stack';
+import { createNativeStackNavigator, NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
 
-import {NavigationHeader} from 'components/content/NavigationHeader';
-import {ForecastScreen} from 'components/screens/ForecastScreen';
-import {MapScreen} from 'components/screens/MapScreen';
-import {NWACObservationScreen, ObservationScreen} from 'components/screens/ObservationsScreen';
-import {StationDetailScreen, StationsDetailScreen} from 'components/screens/WeatherScreen';
-import {HomeStackParamList, TabNavigatorParamList} from 'routes';
+import { NavigationHeader } from 'components/content/NavigationHeader';
+import { ForecastScreen } from 'components/screens/ForecastScreen';
+import { MapScreen } from 'components/screens/MapScreen';
+import { NWACObservationScreen, ObservationScreen, ObservationSubmitScreen } from 'components/screens/ObservationsScreen';
+import { StationDetailScreen, StationsDetailScreen } from 'components/screens/WeatherScreen';
+import { HomeStackParamList, TabNavigatorParamList } from 'routes';
 
 const AvalancheCenterStack = createNativeStackNavigator<HomeStackParamList>();
-export const HomeTabScreen = ({route}: NativeStackScreenProps<TabNavigatorParamList, 'Home'>) => {
-  const {center_id, requestedTime} = route.params;
+export const HomeTabScreen = ({ route }: NativeStackScreenProps<TabNavigatorParamList, 'Home'>) => {
+  const { center_id, requestedTime } = route.params;
   return (
-    <AvalancheCenterStack.Navigator initialRouteName="avalancheCenter" screenOptions={{header: props => <NavigationHeader center_id={center_id} {...props} />}}>
+    <AvalancheCenterStack.Navigator initialRouteName="avalancheCenter" screenOptions={{ header: props => <NavigationHeader center_id={center_id} {...props} /> }}>
       <AvalancheCenterStack.Screen
         name="avalancheCenter"
         component={MapScreen}
-        initialParams={{center_id: center_id, requestedTime: requestedTime}}
-        options={{headerShown: false}}
+        initialParams={{ center_id: center_id, requestedTime: requestedTime }}
+        options={{ headerShown: false }}
       />
-      <AvalancheCenterStack.Screen name="forecast" component={ForecastScreen} initialParams={{center_id: center_id, requestedTime: requestedTime}} options={{headerShown: false}} />
+      <AvalancheCenterStack.Screen name="forecast" component={ForecastScreen} initialParams={{ center_id: center_id, requestedTime: requestedTime }} options={{ headerShown: false }} />
       <AvalancheCenterStack.Screen
         name="stationsDetail"
         component={StationsDetailScreen}
-        options={{title: 'Weather Station', header: props => <NavigationHeader center_id={center_id} {...props} />}}
+        options={{ title: 'Weather Station', header: props => <NavigationHeader center_id={center_id} {...props} /> }}
       />
       <AvalancheCenterStack.Screen
         name="stationDetail"
         component={StationDetailScreen}
-        options={{title: 'Weather Station', header: props => <NavigationHeader center_id={center_id} {...props} />}}
+        options={{ title: 'Weather Station', header: props => <NavigationHeader center_id={center_id} {...props} /> }}
       />
       <AvalancheCenterStack.Screen
         name="observation"
@@ -42,6 +42,13 @@ export const HomeTabScreen = ({route}: NativeStackScreenProps<TabNavigatorParamL
         component={NWACObservationScreen}
         options={{
           title: 'Observation',
+        }}
+      />
+      <AvalancheCenterStack.Screen
+        name="observationSubmit"
+        component={ObservationSubmitScreen}
+        options={{
+          title: 'Submit an Observation'
         }}
       />
     </AvalancheCenterStack.Navigator>
