a3cdefd5bb44abec0989ac1825f1489a6a4c57ac
yuliadub <yulia.dubinina@gmail.com>
2024-10-28 12:31:45 -0700

    prettier

--- components/content/NavigationHeader.tsx
+++ components/content/NavigationHeader.tsx
@@ -1,20 +1,20 @@
-import { AntDesign } from '@expo/vector-icons';
-import { getHeaderTitle } from '@react-navigation/elements';
-import { NativeStackHeaderProps } from '@react-navigation/native-stack/lib/typescript/src/types';
-import { AvalancheCenterLogo } from 'components/AvalancheCenterLogo';
-import { HStack, View } from 'components/core';
-import { Title1Black, Title3Black } from 'components/text';
-import React, { useCallback } from 'react';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { colorLookup } from 'theme';
-import { AvalancheCenterID } from 'types/nationalAvalancheCenter';
+import {AntDesign} from '@expo/vector-icons';
+import {getHeaderTitle} from '@react-navigation/elements';
+import {NativeStackHeaderProps} from '@react-navigation/native-stack/lib/typescript/src/types';
+import {AvalancheCenterLogo} from 'components/AvalancheCenterLogo';
+import {HStack, View} from 'components/core';
+import {Title1Black, Title3Black} from 'components/text';
+import React, {useCallback} from 'react';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {colorLookup} from 'theme';
+import {AvalancheCenterID} from 'types/nationalAvalancheCenter';
 
 export const NavigationHeader: React.FunctionComponent<
   NativeStackHeaderProps & {
     center_id: AvalancheCenterID;
     large?: boolean;
   }
-> = ({ navigation, route, options, back, center_id, large }) => {
+> = ({navigation, route, options, back, center_id, large}) => {
   var share: boolean = false;
   var shareCenterId: AvalancheCenterID = center_id;
   var firstOpen: boolean = false;
@@ -46,7 +46,7 @@ export const NavigationHeader: React.FunctionComponent<
     () =>
       navigation.reset({
         index: 0,
-        routes: [{ name: 'Home' }],
+        routes: [{name: 'Home'}],
       }),
     [navigation],
   );
@@ -54,7 +54,7 @@ export const NavigationHeader: React.FunctionComponent<
   return (
     // On phones with notches, the insets.top value will be non-zero and we don't need additional padding on top.
     // On phones without notches, the insets.top value will be 0 and we don't want the header to be flush with the top of the screen.
-    <View style={{ width: '100%', backgroundColor: 'white', paddingTop: Math.max(8, insets.top) }}>
+    <View style={{width: '100%', backgroundColor: 'white', paddingTop: Math.max(8, insets.top)}}>
       <HStack justifyContent="space-between" pb={8} style={options.headerStyle} space={8} pl={3} pr={16}>
         {back ? (
           <AntDesign.Button
@@ -62,17 +62,17 @@ export const NavigationHeader: React.FunctionComponent<
             color={colorLookup('text')}
             name="arrowleft"
             backgroundColor="white"
-            iconStyle={{ marginLeft: 0, marginRight: 0 }}
-            style={{ textAlign: 'center', borderColor: 'transparent', borderWidth: 1 }}
+            iconStyle={{marginLeft: 0, marginRight: 0}}
+            style={{textAlign: 'center', borderColor: 'transparent', borderWidth: 1}}
             onPress={firstOpen ? reset : goBack}
           />
         ) : (
           <View width={42} />
         )}
-        <TextComponent textAlign="center" style={{ flex: 1, borderColor: 'transparent', borderWidth: 1 }}>
+        <TextComponent textAlign="center" style={{flex: 1, borderColor: 'transparent', borderWidth: 1}}>
           {title}
         </TextComponent>
-        <AvalancheCenterLogo style={{ height: 32, width: 32, resizeMode: 'cover', flex: 0, flexGrow: 0 }} avalancheCenterId={share ? shareCenterId : center_id} />
+        <AvalancheCenterLogo style={{height: 32, width: 32, resizeMode: 'cover', flex: 0, flexGrow: 0}} avalancheCenterId={share ? shareCenterId : center_id} />
       </HStack>
     </View>
   );
