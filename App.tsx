NWACus/avy

commit d0bf3a11981e4120a9f9753dfc2af611c6a9745f

sentry/react-native migration

App.tsx
@@ -149,7 +149,6 @@ if (Sentry?.init) {
       // Example: 1.0.0.54
       dist: `${Application.nativeApplicationVersion || '0.0.0'}.${Application.nativeBuildVersion || '0'}`,
       release: process.env.EXPO_PUBLIC_GIT_REVISION,
-      enableInExpoDevelopment: Boolean(process.env.EXPO_PUBLIC_SENTRY_IN_DEV),
       enableWatchdogTerminationTracking: true,
       beforeSend: async (event, hint) => {
         const {exists} = await FileSystem.getInfoAsync(logFilePath);

commit c3118f70c7b8b699ea052925c77016bfbffb3964

App.tsx: don't send Sentry events in dev unless configured
We regressed this functionality in d0bf3a11981e4120a9f9753dfc2af611c6a9745f.
Following the migration guide to update:

https://docs.sentry.io/platforms/react-native/migration/sentry-expo/#review-sentryinit-options

Signed-off-by: Steve Kuznetsov <stekuznetsov@microsoft.com>

App.tsx
@@ -178,6 +178,7 @@ if (Sentry?.init) {
       // Example: 1.0.0.54
       dist: `${Application.nativeApplicationVersion || '0.0.0'}.${Application.nativeBuildVersion || '0'}`,
       release: process.env.EXPO_PUBLIC_GIT_REVISION as string,
+      enabled: !!Updates.channel || Boolean(process.env.EXPO_PUBLIC_SENTRY_IN_DEV),
       enableWatchdogTerminationTracking: true,
       integrations: [routingInstrumentation],
       beforeSend: async (event, hint) => {