8eb72006bc0f80bb19e73c1d8392921373579ed7
Kevin Hernandez <115322555+kevinherdez@users.noreply.github.com>
2025-11-21 13:10:15 -0800

    Ignore linting for test

--- components/weather_data/WeatherUtils.test.ts
+++ components/weather_data/WeatherUtils.test.ts
@@ -5,6 +5,7 @@ describe('usePartnerCenterIdForStationFetch', () => {
   it('correctly updates the center id when needed', () => {
     for (const center_id in avalancheCenterIDSchema) {
       const centerIdValue = center_id as AvalancheCenterID;
+      // eslint-disable-next-line react-hooks/rules-of-hooks
       const partnerCenterId = usePartnerCenterIdForStationFetch(centerIdValue);
 
       if (centerIdValue == 'HPAC') {
