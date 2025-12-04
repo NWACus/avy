2d0c97db7fa84795bf3962d461874e658f0773d6
Kevin Hernandez <115322555+kevinherdez@users.noreply.github.com>
2025-12-04 14:46:03 -0800

    Fix import cycle

--- components/content/carousel/MediaViewerModal/MediaViewerModalHeader.tsx
+++ components/content/carousel/MediaViewerModal/MediaViewerModalHeader.tsx
@@ -1,14 +1,24 @@
-import {RoundButton} from 'components/content/carousel/MediaViewerModal/MediaViewerModal';
-import {Center, HStack, View} from 'components/core';
+import {AntDesign} from '@expo/vector-icons';
+import {Center, HStack, View, ViewProps} from 'components/core';
 import {BodySm} from 'components/text';
 import React from 'react';
+import {GestureResponderEvent, TouchableOpacity} from 'react-native';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import {colorLookup} from 'theme';
 
 interface MediaViewerModalHeaderProps {
   index: number;
   mediaCount: number;
   onClose: () => void;
 }
+
+const RoundButton = ({onPress, ...props}: {onPress: ((event: GestureResponderEvent) => void) | undefined} & ViewProps) => (
+  <TouchableOpacity onPress={onPress}>
+    <Center height={32} width={32} backgroundColor={colorLookup('modal.background')} borderRadius={16} {...props}>
+      <AntDesign size={24} color="white" name="close" />
+    </Center>
+  </TouchableOpacity>
+);
 
 export const MediaViewerModalHeader: React.FunctionComponent<MediaViewerModalHeaderProps> = ({index, mediaCount, onClose}) => {
   return (
